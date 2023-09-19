c7c7f5f93d48e7238bd0c961565c84691bec197a
daishuming <584359322@qq.com>
2023-09-19 18:56:34 +0800

    新增用户管理页面

--- src/views/systemoperate/user.vue
+++ src/views/systemoperate/user.vue
@@ -2,9 +2,7 @@
 	<div>
 		<div class="container">
 			<div class="handle-box">
-				<el-input v-model="query.menuName" placeholder="菜单名称" class="handle-input mr10"></el-input>
-				<el-input v-model="query.menuLevel" placeholder="层级" class="handle-input mr10"></el-input>
-				<el-input v-model="query.componentUrl" placeholder="url" class="handle-input mr10"></el-input>
+				<el-input v-model="query.userName" placeholder="用户名称" class="handle-input mr10"></el-input>
 				<el-select v-model="query.status" class="m-2" placeholder="状态">
 					<el-option v-for="item in dics.status" :key="item.key" :label="item.value" :value="item.key" />
 				</el-select>
@@ -20,21 +18,17 @@
 				<el-button type="primary" :icon="Plus" @click="add" v-if=editAuth>新增</el-button>
 			</div>
 
-			<el-table :data="tableData" border class="table" ref="multipleTable" header-cell-class-name="table-header"
-				:row-class-name="tableRowClassName">
-				<el-table-column prop="menuId" label="ID" width="55" align="center"></el-table-column>
-				<el-table-column prop="menuName" label="菜单名称"></el-table-column>
-				<el-table-column prop="menuLevel" label="层级" width="60"></el-table-column>
-				<el-table-column prop="componentUrl" label="url" width="300"></el-table-column>
+			<el-table :data="tableData" border class="table" ref="multipleTable" header-cell-class-name="table-header">
+				<el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
+				<el-table-column prop="userName" label="用户名称"></el-table-column>、
 				<el-table-column prop="status" label="状态">
 					<template #default="scope">
 						<div :style="getStatusStyle(scope.row.status)">{{ transformDics(dics.status, scope.row.status) }}
 						</div>
 					</template>
 				</el-table-column>
-				<el-table-column prop="sortNo" label="排序"></el-table-column>
-				<el-table-column prop="inputTime" label="录入时间" width="120"></el-table-column>
-				<el-table-column prop="updateTime" label="更新时间" width="120"></el-table-column>
+				<el-table-column prop="inputTime" label="录入时间"></el-table-column>
+				<el-table-column prop="updateTime" label="更新时间"></el-table-column>
 				<el-table-column label="操作" width="220" align="center" v-if=editAuth>
 					<template #default="scope">
 						<el-button text :icon="Edit" @click="handleEdit(scope.$index, scope.row)" v-if=editAuth>
@@ -56,37 +50,13 @@
 		<el-dialog title="编辑" v-model="editVisible" width="40%">
 			<el-form :model="form" :rules="rules" ref="editForm" label-width="110px">
 				<el-form-item label="id" v-if="false">
-					<el-input v-model="form.menuId" disabled></el-input>
+					<el-input v-model="form.id" disabled></el-input>
 				</el-form-item>
-				<el-form-item label="菜单名称" prop="menuName">
-					<el-input v-model="form.menuName" placeholder=""></el-input>
-				</el-form-item>
-				<el-form-item label="上级菜单ID" prop="upMenuId">
-					<el-input v-model="form.upMenuId"></el-input>
-				</el-form-item>
-				<el-form-item label="层级" prop="menuLevel">
-					<el-input v-model="form.menuLevel"></el-input>
-				</el-form-item>
-				<el-form-item label="菜单url" prop="menuUrl">
-					<el-input v-model="form.menuUrl"></el-input>
-				</el-form-item>
-				<el-form-item label="路由名称" prop="routeName">
-					<el-input v-model="form.routeName"></el-input>
-				</el-form-item>
-				<el-form-item label="菜单类型" prop="menuType">
-					<el-input v-model="form.menuType"></el-input>
-				</el-form-item>
-				<el-form-item label="icon" prop="icon">
-					<el-input v-model="form.icon"></el-input>
-				</el-form-item>
-				<el-form-item label="页面路径" prop="componentUrl">
-					<el-input v-model="form.componentUrl"></el-input>
+				<el-form-item label="用户名称" prop="userName">
+					<el-input v-model="form.userName" placeholder=""></el-input>
 				</el-form-item>
 				<el-form-item label="状态" prop="status" v-if="false">
 					<el-input v-model="form.status"></el-input>
-				</el-form-item>
-				<el-form-item label="排序" prop="sortNo">
-					<el-input v-model="form.sortNo"></el-input>
 				</el-form-item>
 			</el-form>
 			<template #footer>
@@ -103,7 +73,7 @@
 import { ref, reactive, stop } from 'vue';
 import { ElMessage, ElMessageBox } from 'element-plus';
 import { Delete, Edit, Search, Plus } from '@element-plus/icons-vue';
-import { fetchData, insert, update, deleteData } from '../../api/menu';
+import { fetchData, insert, update, deleteData } from '../../api/user';
 import { errorInfo } from '../../constants/error';
 import { inject } from 'vue-demi';
 import type { FormInstance, FormRules } from 'element-plus';
@@ -111,53 +81,22 @@ const reload = inject('reload') as { reload: () => void };
 const editAuth = localStorage.getItem('editAuth') === 'true';
 import { queryLibraries } from '../../api/codelibrary';
 interface TableItem {
-	menuId: number,
+	id: number,
 	upMenuId: number,
-	menuLevel: number,
-	menuUrl: string,
-	menuName: string,
-	routeName: string,
-	menuType: string,
-	icon: string,
-	componentUrl: string,
+	userName: string,
 	status: string,
-	sortNo: string,
 	inputUser: string,
 	inputTime: string,
 	updateUser: string,
 	updateTime: string
 }
 const rules: FormRules = {
-	menuName: [
-		{ required: true, message: "请输入菜单名称" }
-	],
-	// upMenuId: [
-	// 	{ required: true, message: "请输入上级菜单id" }
-	// ],
-	menuLevel: [
-		{ required: true, message: "请输入层级" }
-	],
-	menuUrl: [
-		{ required: true, message: "请输入url" }
-	],
-	routeName: [
-		{ required: true, message: "请输入路由名称" }
-	],
-	// menuType: [
-	// 	{ required: true, message: "请输入菜单类型" }
-	// ],
-	componentUrl: [
-		{ required: true, message: "请输入vue路径" }
-	],
-	sortNo: [
-		{ required: true, message: "请输入排序号" }
+	userName: [
+		{ required: true, message: "请输入用户名称" }
 	]
-
 };
 const query = reactive({
-	menuName: '',
-	menuLevel: '',
-	componentUrl: '',
+	userName: '',
 	status: '',
 	pageIndex: 1,
 	pageSize: 10
@@ -183,24 +122,16 @@ const handleSearch = () => {
 };
 // 重置操作
 const clearQuery = () => {
-	query.menuName = '';
+	query.userName = '';
 	query.pageIndex = 1;
 	getData();
 };
 //新增操作
 const add = () => {
 	editVisible.value = true;
-	form.menuId = '',
-		form.upMenuId = '',
-		form.menuLevel = '',
-		form.menuUrl = '',
-		form.menuName = '',
-		form.routeName = '',
-		form.menuType = '',
-		form.icon = '',
-		form.componentUrl = '',
+	form.id = '',
+		form.userName = '',
 		form.status = '',
-		form.sortNo = '',
 		form.inputUser = '',
 		form.inputTime = '',
 		form.updateUser = '',
@@ -253,7 +184,7 @@ const handleDelete = (row: any) => {
 		type: 'warning'
 	})
 		.then(() => {
-			form.menuId = row.menuId;
+			form.id = row.id;
 			form.status = row.status;
 			deleteData(
 				form
@@ -274,17 +205,9 @@ const handleDelete = (row: any) => {
 const editVisible = ref(false);
 const insertOrUpdate = ref('')
 let form = reactive({
-	menuId: '',
-	upMenuId: '',
-	menuLevel: '',
-	menuUrl: '',
-	menuName: '',
-	routeName: '',
-	menuType: '',
-	icon: '',
-	componentUrl: '',
+	id: '',
+	userName: '',
 	status: '',
-	sortNo: '',
 	inputUser: '',
 	inputTime: '',
 	updateUser: '',
@@ -315,19 +238,6 @@ const transformDics = (dic: { key: string; value: string }[], key: string): stri
 	const item = dic.find(item => item.key === key);
 	return item ? item.value : "";
 };
-
-const tableRowClassName = ({
-	row,
-	rowIndex,
-}: {
-	row: TableItem
-	rowIndex: number
-}) => {
-	if (row.menuLevel === 1) {
-		return 'level-one'
-	}
-	return ''
-}
 const getStatusStyle = (status: string) => {
 	if (status === "0") {
 		return { color: 'red' };
@@ -367,9 +277,5 @@ const getStatusStyle = (status: string) => {
 	margin: auto;
 	width: 40px;
 	height: 40px;
-}
-
-:deep(.el-table .level-one) {
-	--el-table-tr-bg-color: var(--el-fill-color-darker);
 }
 </style>
