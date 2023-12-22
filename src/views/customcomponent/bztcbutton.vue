dc7802c27fafc044ea3384bd9394dd244adbc5a1
daishuming <584359322@qq.com>
2023-12-22 17:36:14 +0800

    修改权限控制方法

--- src/views/customcomponent/bztcbutton.vue
+++ src/views/customcomponent/bztcbutton.vue
@@ -3,6 +3,20 @@
 </template>
 <script setup lang="ts">
 const props = defineProps(['buttonName', 'icon', 'type', 'controlKey']);
-import getControlVisiable from '../../method/bztccontrol';
+const getControlVisiable = (controlKey: string) => {
+    if (controlKey === null || controlKey.length < 1) {
+        return false
+    }
+    let controlInfoStr = localStorage.getItem('control_info')
+    if (controlInfoStr !== null && controlInfoStr.length > 0 && controlInfoStr !== undefined && controlInfoStr !== "null") {
+        let controlInfo = JSON.parse(controlInfoStr!)
+        let controlKeys = controlInfo.map((item: any) => item.controlKey)
+        if (controlKeys.includes(controlKey)) {
+            return true
+        } else {
+            return false
+        }
+    }
+};
 const visiable = props.controlKey ? getControlVisiable(props.controlKey) : false
 </script>
