e0c35e1b933092a8d57ccaad1c9a626e333fbd6e
daishuming <584359322@qq.com>
2023-12-25 10:42:09 +0800

    更改chatgpt地址

--- src/views/chatgpt.vue
+++ src/views/chatgpt.vue
@@ -1,9 +1,8 @@
 <template>
 	<div>
     <!-- 使用 iframe 标签嵌入网页 -->
-    <iframe src="http://bztc.website:3000/#/chat" width="100%" height="1000px" frameborder="0"></iframe>
+    <iframe src="https://bztc.website:3000/#/chat" width="100%" height="1000px" frameborder="0"></iframe>
   </div>
 </template>
-
 <script setup lang="ts" name="md">
 </script>
